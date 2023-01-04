2ef5927e68c1affbb0f366e4424c4f177112ab01
EnesK019 <eneskrtnc.19@hotmail.com>
2023-01-04 12:24:12 +0300

    10.Gün bitti

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -37,31 +37,35 @@ namespace Business.Concrete
         {
             if (DateTime.Now.Hour == 22)
             {
-                return new ErrorDataResult();
+                return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
 
-            return new SuccessDataResult<Product>>(_productDal.GetAll(), true, "Ürünler listelendi");
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(), Messages.ProductListed);
             
         }
 
         public IDataResult<List<Product>> GetAllByCategoryId(int id)
         {
-            return _productDal.GetAll(p=>p.CategoryId == id);
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p=>p.CategoryId == id));
         }
 
         public IDataResult<Product> GetById(int id)
         {
-            return _productDal.Get(p=>p.ProductId== id);
+            return new SuccessDataResult<Product>(_productDal.Get(p=>p.ProductId== id));
         }
 
         public IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max)
         {
-            return _productDal.GetAll(p=>p.UnitPrice>=min && p.UnitPrice<=max);
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p=>p.UnitPrice>=min && p.UnitPrice<=max));
         }
 
         public IDataResult<List<ProductDetailDto>> GetProductDetails()
         {
-            return _productDal.GetProductDetails();
+            if (DateTime.Now.Hour == 12)
+            {
+                return new ErrorDataResult<List<ProductDetailDto>>(Messages.MaintenanceTime);
+            }
+            return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetails());
         }
     }
 }
--- Core/Utilities/Results/ErrorDataResult.cs
+++ Core/Utilities/Results/ErrorDataResult.cs
@@ -4,7 +4,26 @@ using System.Text;
 
 namespace Core.Utilities.Results
 {
-    internal class ErrorDataResult
+    public class ErrorDataResult<T> : DataResult<T>
     {
+        public ErrorDataResult(T data, string message) : base(data, false, message)
+        {
+
+        }
+
+        public ErrorDataResult(T data) : base(data, false)
+        {
+
+        }
+
+        public ErrorDataResult(string message) : base(default, false, message)
+        {
+
+        }
+
+        public ErrorDataResult() : base(default, false)
+        {
+
+        }
     }
 }
