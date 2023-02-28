34f6a1ee269b436ac8c5539c1f7781486afb80c8
EnesK019 <eneskrtnc.19@hotmail.com>
2023-02-28 13:31:09 +0300

    15.gün 1.Mola

--- Business/ValidationRules/FluentValidation/ProductValidator.cs
+++ Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -15,7 +15,7 @@ namespace Business.ValidationRules.FluentValidation
             RuleFor(p => p.UnitPrice).NotEmpty();
             RuleFor(p => p.UnitPrice).GreaterThan(0);
             RuleFor(p => p.UnitPrice).GreaterThan(10).When(p => p.CategoryId == 1);
-            RuleFor(p => p.ProductName).Must(startwitha).WithMessage("ürünler a harfi ile başlamalı");
+           // RuleFor(p => p.ProductName).Must(startwitha).WithMessage("ürünler a harfi ile başlamalı");
         }
 
         private bool startwitha(string arg)
